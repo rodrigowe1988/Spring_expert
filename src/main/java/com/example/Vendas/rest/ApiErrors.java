f05492cd45651c6906748e0d201e4c8e44fd80d3
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2023-03-26 12:31:59 -0300

    utilizando CONTROLLER ADVICE e EXCEPTIONHANDLERS para tratar os erros da API

---
+++ src/main/java/com/example/Vendas/rest/ApiErrors.java
@@ -0,0 +1,19 @@
+package com.example.Vendas.rest;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String mensagemErro) {
+        this.errors = Arrays.asList(mensagemErro);
+    }
+
+
+}
--- src/main/java/com/example/Vendas/rest/controller/PedidoController.java
+++ src/main/java/com/example/Vendas/rest/controller/PedidoController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
